eb7bfc619ca75d81bdeae4373c839a708929965c
startling <tdixon51793@gmail.com>
2013-04-13 16:03:38 -0400

    namespace the runtime.

--- Beetle/Compile.hs
+++ Beetle/Compile.hs
@@ -9,8 +9,18 @@ import Language.Javascript
 declaration :: Expression -> B.Declaration -> Statement
 declaration e (B.Declaration t a) = Var t . Just $ expression e a
 
+runtime :: Text -> Expression
+runtime = Attribute (Variable "beetle")
+
 expression :: Expression -> B.Abstract -> Expression
-expression e (B.Symbol t) = Variable t
+expression e (B.Symbol t) = case t of
+    "switch-to" -> runtime "switch_to"
+    "if" -> runtime "if_"
+    t -> if t `elem` functions then runtime t else Variable t
+  where
+    functions :: [Text]
+    functions = ["paragraph", "field", "link", "exec"]
+    -- TODO: generalized transform to js-safe identifiers
 expression e (B.Literal t) = Literal t
 expression e (B.Call a b) = Call (expression e a) [e, expression e b]
 expression e (B.Block ss) = Function ["element"]
@@ -22,7 +32,7 @@ expression e (B.Dict ss) = Object (map (fmap $ expression e) ss)
 statement :: Expression -> B.Statement -> Statement
 statement e (B.Reassignment m a) = Reassign m $ expression e a
 statement e (B.Assignment m a) = Var m . Just $ expression e a
-statement e (B.Paragraph ts) = Expression $ Call (Variable "paragraph")
+statement e (B.Paragraph ts) = Expression $ Call (runtime "paragraph")
   [ e
   , foldr (Operator "+") (Literal "")
      $ map (either Literal $ expression e) ts
--- runtime.js
+++ runtime.js
@@ -2,48 +2,50 @@ $(document).ready(function () {
   start($("#content"));
 });
 
-function switchto (element, fn) {
-  element.empty();
-  fn(element);
-};
+var beetle = {
+  switch_to: function (element, fn) {
+    element.empty();
+    fn(element);
+  },
 
-function paragraph (element, string) {
-  var $par = $("<p/>");
-  $par.append(document.createTextNode(string));
-  element.append($par);
-}
+  paragraph: function (element, string) {
+    var $par = $("<p/>");
+    $par.append(document.createTextNode(string));
+    element.append($par);
+  },
 
-function field (element, fn) {
-  var $form = $("<form/>");
-  var $text = $("<input/>", {type : "text"});
-  $form.append($text);
-  $form.submit(function () {
-    fn(element, $text.val());
-    return false;
-  });
-  element.append($form);
-};
-
-function link (_, f) {
-  return function (element, g) {
-    var $a = $('<a/>');
-    $a.addClass("link");
-    $a.click(function () {
-      g(element);
+  field: function (element, fn) {
+    var $form = $("<form/>");
+    var $text = $("<input/>", {type : "text"});
+    $form.append($text);
+    $form.submit(function () {
+      fn(element, $text.val());
+      return false;
     });
-    f($a);
-    element.append($a);
-  };
-};
+    element.append($form);
+  },
 
-function exec (element, f) {
-  f(element);
-};
-
-function if_ (_, c) {
-  return function (_, f) {
+  link: function (_, f) {
     return function (element, g) {
-      if (c) { f(element); } else { g(element); };
+      var $a = $('<a/>');
+      $a.addClass("link");
+      $a.click(function () {
+        g(element);
+      });
+      f($a);
+      element.append($a);
+    };
+  },
+
+  exec: function (element, f) {
+    f(element);
+  },
+
+  if_: function (_, c) {
+    return function (_, f) {
+      return function (element, g) {
+        if (c) { f(element); } else { g(element); };
+      }
     }
   }
 };
