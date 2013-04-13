1585a550050129d3d2699df423b39ef07ad7b459
startling <tdixon51793@gmail.com>
2013-04-12 20:29:02 -0400

    some dumb mistakes in the pretty-printer (mostly parens).

--- Language/Javascript.hs
+++ Language/Javascript.hs
@@ -23,16 +23,16 @@ data Expression
 
 printExpression :: Expression -> Text
 printExpression (Call e es) = "((" <> printExpression e <> ")("
-  <> T.intercalate ", " (map printExpression es)
+  <> T.intercalate ", " (map printExpression es) <> "))"
 printExpression (Function ps ss) = "function ("
   <> T.intercalate ", " ps <> "){\n"
-  <> T.intercalate "\n" (map printStatement ss) <> "}"
+  <> T.intercalate "\n" (map printStatement ss) <> "\n}"
 printExpression (Object _) = "{ todo: \"this\" }"
 printExpression (Attribute e t) = printExpression e <> "." <> t
 printExpression (Array _) = "[ \"todo\" ]"
 printExpression (Number _) = "1337"
 printExpression (Variable t) = t
-printExpression (Literal _) = "\"todo\""
+printExpression (Literal t) = "\"" <> t <> "\"" -- escape!
 
 data Statement
   = Expression Expression
