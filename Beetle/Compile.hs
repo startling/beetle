9eb77f99efc7e4c721f744f100c122ef7ca44c3a
startling <tdixon51793@gmail.com>
2013-04-13 17:09:59 -0400

    cleaned up Beetle.Compile

--- Beetle/Compile.hs
+++ Beetle/Compile.hs
@@ -6,12 +6,24 @@ import Data.Text (Text)
 import qualified Beetle.Abstract as B
 import Language.Javascript
 
+-- | Create a top-level Javascript declaration from a Beetle one.
 declaration :: Expression -> B.Declaration -> Statement
 declaration e (B.Declaration t a) = Var t . Just $ expression e a
 
+-- | Create a Javascript function from a list of parameters and some
+-- Beetle statements.
+function :: [Text] -> [B.Statement] -> Expression
+function ps ss = Function ("element":ps) . onLast Return
+  $ map (statement $ Variable "element") ss where
+    onLast _ [] = []
+    onLast f (a : []) = f a : []
+    onLast f (a : as) = a : onLast f as
+
+-- | An 'Expression' representing the runtime function with some name.
 runtime :: Text -> Expression
 runtime = Attribute (Variable "beetle")
 
+-- | Compile a Beetle expression to a Javascript one.
 expression :: Expression -> B.Abstract -> Expression
 expression e (B.Symbol t) = case t of
     "switch-to" -> runtime "switch_to"
@@ -24,18 +36,11 @@ expression e (B.Symbol t) = case t of
 expression e (B.Attribute a s) = Attribute (expression e a) s
 expression e (B.Literal t) = Literal t
 expression e (B.Call a b) = Call (expression e a) [e, expression e b]
-expression e (B.Block ss) = Function ["element"] . onLast Return
-  $ map (statement $ Variable "element") ss where
-    onLast _ [] = []
-    onLast f (a : []) = f a : []
-    onLast f (a : as) = a : onLast f as
-expression e (B.Fn p ss) = Function ["element", p] . onLast Return
-  $ map (statement $ Variable "element") ss where
-    onLast _ [] = []
-    onLast f (a : []) = f a : []
-    onLast f (a : as) = a : onLast f as
+expression e (B.Block ss) = function [] ss
+expression e (B.Fn p ss) = function [p] ss
 expression e (B.Dict ss) = Object (map (fmap $ expression e) ss)
 
+-- | Compile a Beetle statement to a Javascript one.
 statement :: Expression -> B.Statement -> Statement
 statement e (B.Reassignment m a) = Reassign m $ expression e a
 statement e (B.Assignment m a) = Var m . Just $ expression e a
