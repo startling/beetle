5c349109a28e780cc098c1f8b3e93561a548673f
startling <tdixon51793@gmail.com>
2013-04-12 20:06:36 -0400

    rudimentary pretty-printing.

--- Language/Javascript.hs
+++ Language/Javascript.hs
@@ -1,7 +1,10 @@
-{-# Language Rank2Types #-}
+{-# Language OverloadedStrings #-}
 module Language.Javascript where
+-- base
+import Data.Monoid
 -- text
 import Data.Text (Text)
+import qualified Data.Text as T
 
 data Expression
   = Literal Text
@@ -18,11 +21,23 @@ data Expression
   , Show
   )
 
+printExpression :: Expression -> Text
+printExpression (Call e es) = "((" <> printExpression e <> ")("
+  <> T.intercalate ", " (map printExpression es)
+printExpression (Function ps ss) = "function ("
+  <> T.intercalate ", " ps <> "){\n"
+  <> T.intercalate "\n" (map printStatement ss) <> "}"
+printExpression (Object _) = "{ todo: \"this\" }"
+printExpression (Attribute e t) = printExpression e <> "." <> t
+printExpression (Array _) = "[ \"todo\" ]"
+printExpression (Number _) = "1337"
+printExpression (Variable t) = t
+printExpression (Literal _) = "\"todo\""
+
 data Statement
   = Expression Expression
   | Reassign Text Expression
   | Var Text (Maybe Expression)
-  | If Expression Block [(Expression, Block)] (Maybe Block)
   deriving
   ( Eq
   , Ord
@@ -30,3 +45,10 @@ data Statement
   )
 
 type Block = [Statement]
+
+printStatement :: Statement -> Text
+printStatement (Expression e) = printExpression e <> ";"
+printStatement (Reassign t e) = t <> " = " <> printExpression e <> ";"
+printStatement (Var t Nothing) = "var " <> t <> ";"
+printStatement (Var t (Just e)) = "var " <> t
+  <> " = " <> printExpression e <> ";"
