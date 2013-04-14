6f150b408f53980fe0021105fdc0413e57df4738
startling <tdixon51793@gmail.com>
2013-04-14 17:35:19 -0400

    more sophisticated javascript types + rendering.

--- Language/Javascript.hs
+++ Language/Javascript.hs
@@ -1,69 +1,91 @@
+{-# Language DeriveFunctor #-}
+{-# Language DeriveFoldable #-}
+{-# Language DeriveTraversable #-}
 {-# Language OverloadedStrings #-}
 module Language.Javascript where
 -- base
-import Data.Monoid
+import Control.Applicative
 import Data.Char
+import Data.List
+import Data.Foldable (Foldable)
+import Data.Traversable (Traversable)
 import Text.Printf
--- text
+-- text 
 import Data.Text (Text)
 import qualified Data.Text as T
+import Data.Text.Lazy.Builder
+-- transformers
+import Data.Functor.Identity
+-- mtl
+import Control.Monad.Reader
+import Control.Monad.Writer
 
-data Expression
-  = Literal Text
-  | Variable Text
-  | Number Rational
-  | Array [Expression]
-  | Attribute Expression Text
-  | Object [(Text, Expression)]
-  | Function [Text] [Statement]
-  | Call Expression [Expression]
-  | Operator Text Expression Expression
+data Expression v
+  = Variable v
+  | Literal Text
+  | Object [(Text, Expression v)]
+  | Attribute Text (Expression v)
+  | FunctionExp (Function v)
+  | Call (Expression v) [Expression v]
   deriving
   ( Eq
   , Ord
   , Show
+  , Functor
+  , Foldable
+  , Traversable
   )
 
-printExpression :: Expression -> Text
-printExpression (Call e es) = "((" <> printExpression e <> ")("
-  <> T.intercalate ", " (map printExpression es) <> "))"
-printExpression (Function ps ss) = "function ("
-  <> T.intercalate ", " ps <> "){\n"
-  <> T.intercalate "\n" (map printStatement ss) <> "\n}"
-printExpression (Object []) = "{}"
-printExpression (Object ds) = "{\n    "
-  <> T.intercalate "\n  , " (map line ds) <> "\n}" where
-    line (a, b) = a <> " : " <> printExpression b
-printExpression (Attribute e t) = "(" <> printExpression e <> ")." <> t
-printExpression (Array as) = "[" <>
-  T.intercalate ", " (map printExpression as) <> "]"
-printExpression (Number _) = "1337"
-printExpression (Variable t) = t
-printExpression (Literal t) = "\"" <> escape t <> "\""
-printExpression (Operator t a b) = "((" <> printExpression a <> ") "
-  <> t <> " (" <> printExpression b <> "))"
-
-data Statement
-  = Return Statement
-  | Expression Expression
-  | Reassign Text [Text] Expression
-  | Var Text (Maybe Expression)
+data Statement v
+  = Return (Expression v)
+  | Assign v (Expression v)
+  | Expression (Expression v)
   deriving
   ( Eq
   , Ord
   , Show
+  , Functor
+  , Foldable
+  , Traversable
   )
 
-type Block = [Statement]
+data Block v = Block
+  { provides   :: [v]
+  , statements :: [Statement v] }
+  deriving
+  ( Eq
+  , Ord
+  , Show
+  , Functor
+  , Foldable
+  , Traversable
+  )
 
-printStatement :: Statement -> Text
-printStatement (Return e) = "return " <> printStatement e
-printStatement (Expression e) = printExpression e <> ";"
-printStatement (Reassign t as e) = T.intercalate "." (t : as)
-  <> " = " <> printExpression e <> ";"
-printStatement (Var t Nothing) = "var " <> t <> ";"
-printStatement (Var t (Just e)) = "var "
-  <> t <> " = " <> printExpression e <> ";"
+data Function v = Function
+  { parameters :: [v]
+  , body       :: Block v
+  }
+  deriving
+  ( Eq
+  , Ord
+  , Show
+  , Functor
+  , Foldable
+  , Traversable
+  )
+
+type RenderT m = ReaderT Int (WriterT Builder m)
+type Render = RenderT Identity
+
+indented :: Monad m => RenderT m a -> RenderT m a
+indented = local (+ 1)
+
+line x = indent `liftM` ask >>= tell
+  >> x >> tell "\n" where
+    indent i = fromText $ T.replicate i "  "
+
+word :: Monad m => Text-> RenderT m ()
+word = tell .fromText
 
 escape :: Text -> Text
 escape = T.concatMap $ \x -> case x of
@@ -72,6 +94,54 @@ escape = T.concatMap $ \x -> case x of
   '\\' -> "\\\\";
   c -> if isPrint c then T.singleton c
     else T.pack $ printf "\\u%04x" $ ord c
+
+expression :: Monad m => Expression Text -> RenderT m ()
+expression (Variable t) = word t
+expression (Literal l) = word $ "\"" <> escape l <> "\""
+expression (Object []) = word "({})"
+expression (Object o) = braces . indented $ mapM_ each o where
+  braces :: Monad m => RenderT m a -> RenderT m ()
+  braces b = word "({\n" >> b >> word "})"
+  each :: Monad m => (Text, Expression Text) -> RenderT m ()
+  each (k, v) = line (word $ "\"" <> escape k <> "\" : ") >> expression v
+expression (Attribute t e) = expression e >> word "." >> word t
+expression (Call f []) = expression f >> word "()"
+expression (Call f (a : as)) = expression f
+  >> parens (expression a >> forM_ as each) where
+    parens :: Monad m => RenderT m a -> RenderT m ()
+    parens b = word "(" >> b >> word ")"
+    each :: Monad m => Expression Text -> RenderT m ()
+    each x = word "," >> expression x
+expression (FunctionExp f) = function f
+
+statement :: Monad m => Statement Text -> RenderT m ()
+statement (Assign v e) = line (word v >> word " = " >> expression e >> word ";")
+statement (Return e) = line (word "return " >> expression e >> word ";")
+statement (Expression e) = line (expression e >> word ";")
+
+block :: Monad m => Block Text -> RenderT m ()
+block (Block ps ss) = forM_ ps var >> mapM_ statement ss where
+  var t = line $ word "var " >> word t >> word ";"
+
+function :: Monad m => Function Text -> RenderT m ()
+function (Function ps b) = parens $ do
+    line $ word "function" >> parens (commas $ ps) >> word " {"
+    indented $ block b
+    word "}"
+  where
+    parens :: Monad m => RenderT m a -> RenderT m ()
+    parens v = word "(" >> v >> word ")"
+    commas :: Monad m => [Text] -> RenderT m ()
+    commas [] = word ""
+    commas (a : []) = word a
+    commas (a : as) = word a >> forM_ as
+      (\e -> word "," >> word e)
+
+runRenderT :: Monad m => RenderT m a -> m Builder
+runRenderT r = execWriterT $ runReaderT r 0
+
+runRender :: RenderT Identity a -> Builder
+runRender = runIdentity . runRenderT
 
 keywords :: [Text]
 keywords =
