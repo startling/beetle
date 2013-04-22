a0f3d065f25de401376048c8ae10faef451fd519
startling <tdixon51793@gmail.com>
2013-04-22 03:06:24 -0400

    Compile Beetle to the new javascript ast.

--- Beetle/Compile.hs
+++ Beetle/Compile.hs
@@ -8,57 +8,79 @@ import qualified Beetle.Abstract as B
 import Language.Javascript (Expression(..), LHS(..)
   , Statement(..), Block(..), Function(..), keywords)
 
+data V
+  = Passing
+  | Introducing Text
+  deriving
+  ( Eq
+  , Ord
+  , Show
+  )
+
+data E
+  = Element
+  | Symbol Text
+  | Runtime Text
+  deriving
+  ( Eq
+  , Ord
+  , Show
+  )
+
 -- | Create a top-level Javascript declaration from a set of Beetle ones.
-declarations :: [B.Declaration] -> Block Text a
+declarations :: [B.Declaration] -> Block V E
 declarations es = Block (map name es) (map assign es) where
-  name :: B.Declaration -> Text
-  name (B.Declaration t _) = t
-  assign :: B.Declaration -> Statement Text a
+  name :: B.Declaration -> V
+  name (B.Declaration t _) = Introducing t
+  assign :: B.Declaration -> Statement V E
   assign (B.Declaration t e) = Expression .
-    Assign (LVariable t) $ expression e
+    Assign (LVariable (Introducing t)) $ expression e
 
--- | Compile a Beetle Block to a Javascript one.
-block :: [B.Statement] -> Block Text a
-block ss = Block (ss >>= locals) (last ret $ map statement ss) where
-  locals (B.Assignment t _) = [t]
-  locals otherwise = []
-  statement :: B.Statement -> Statement Text a
-  statement (B.Splice e) = Expression $ expression e
-  statement (B.Assignment t e) = Expression
-    . Assign (LVariable t) $ expression e
-  statement (B.Reassignment l e) = Expression . Assign
-    (lhs l) $ expression e where
-      lhs :: B.LHS -> LHS Text a 
-      lhs (B.LSymbol t) = LVariable $ mangle t
-      lhs (B.LAttribute t e) = LAttribute (mangle t) (expression e)
-  statement (B.Paragraph (e : [])) = Expression
-    $ Call (runtime "paragraph")
-      [element, either Literal expression e]
-  statement (B.Paragraph es) = Expression
-    $ Call (runtime "paragraph")
-      [element, flip Call [Literal ""] . Attribute "join" . Array
-        $ map (either Literal expression) es
-      ]
-  last :: (a -> a) -> [a] -> [a]
-  last _ [] = []
-  last f (b : []) = f b : []
-  last f (b : bs) = b : last f bs
-  ret :: Statement a b -> Statement a b
-  ret (Expression e) = Return e
-  ret otherwise = otherwise
-
-expression :: B.Expression -> Expression Text a
-expression (B.Symbol t) = if t `elem` provided then
-   runtime t else Variable $ mangle t
-expression (B.Literal t) = Literal t
-expression (B.Block b) = FunctionExp . Function ["element"] $ block b
-expression (B.Fn a b) = FunctionExp . Function ["element", mangle a]
-  . Block [] . return . Return  . FunctionExp
-  . Function ["element"] $ block b
-expression (B.Call a b) = Call (expression a) [element, expression b]
+-- | Compile an expression in Beetle to one in Javascript.
+expression :: B.Expression -> Expression V E
+expression (B.Attribute t e) = Attribute t $ expression e
 expression (B.Dict os) = Object $ map (fmap expression) os
-expression (B.Attribute t e) = Attribute (mangle t) $ expression e
+expression (B.Call e a) = Call (expression e) (return $ expression a)
+expression (B.Fn t ss) = FunctionExp . Function [Passing, Introducing t]
+  . Block [] . return . Return . FunctionExp
+  . Function [Passing] $ block ss
+expression (B.Block ss) = FunctionExp . Function [Passing] $ block ss
+expression (B.Literal t) = Literal t
+expression (B.Symbol v) = Other $ Symbol v
 
+-- | Compile a statement in Beetle to some in Javascript.
+statement :: B.Statement -> [Statement V E]
+statement (B.Splice e) = [Expression $ expression e]
+statement (B.Assignment t e) = return . Expression
+  . Assign (LVariable (Introducing t)) $ expression e
+statement (B.Reassignment l e) = return . Expression
+  . Assign (lhs l) $ expression e where
+    lhs (B.LSymbol t) = LVariable $ Introducing t
+    lhs (B.LAttribute t e) = LAttribute t $ expression e
+statement (B.Paragraph []) = return . Expression
+  . Call (Other $ Runtime "paragraph") $ [Literal ""]
+statement (B.Paragraph (e : [])) = return . Expression
+  $ Call (Other $ Runtime "paragraph")
+     [Other Element, either Literal expression e]
+statement (B.Paragraph es) = return . Expression
+  $ Call (Other $ Runtime "paragraph")
+    [ Other Element
+    , flip Call [Literal ""] . Attribute "join" . Array
+      . (`map` es) $ either Literal expression
+    ]
+
+-- | Compile a Beetle block to a Javascript one.
+block :: [B.Statement] -> Block V E
+block ss = Block (ss >>= locals) (last $ ss >>= statement)
+  where
+    locals :: B.Statement -> [V]
+    locals (B.Assignment t _) = [Introducing t]
+    locals _ = []
+    last :: [Statement v o] -> [Statement v o]
+    last [] = []
+    last (Expression e : []) = Return e : []
+    last (a : as) = a : last as
+    
 element :: Expression Text a
 element = Variable "element"
 
