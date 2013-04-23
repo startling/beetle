6e4131e76e39dfce6d3d006edfe6e21270fe36d3
startling <tdixon51793@gmail.com>
2013-04-23 15:29:38 -0400

    Bitraversable instances for everything.

--- Language/Javascript.hs
+++ Language/Javascript.hs
@@ -8,7 +8,7 @@ import Control.Applicative
 import Data.Char
 import Data.List
 import Data.Foldable (Foldable)
-import Data.Traversable (Traversable)
+import Data.Traversable (Traversable, traverse)
 import Text.Printf
 -- text 
 import Data.Text (Text)
@@ -19,6 +19,10 @@ import Data.Functor.Identity
 -- mtl
 import Control.Monad.Reader
 import Control.Monad.Writer
+-- bitraversable
+import Data.Bifunctor
+import Data.Bifoldable
+import Data.Bitraversable
 
 data Expression v o
   = Variable v
@@ -39,6 +43,26 @@ data Expression v o
   , Traversable
   )
 
+instance Bifunctor Expression where
+  bimap = bimapDefault
+
+instance Bifoldable Expression where
+  bifoldMap = bifoldMapDefault
+
+instance Bitraversable Expression where
+  bitraverse f g (Variable v) = Variable <$> f v
+  bitraverse _ _ (Literal t) = pure $ Literal t
+  bitraverse f g (Object os) = Object <$> traverse
+    (bitraverse pure $ bitraverse f g) os
+  bitraverse f g (Attribute t e) = Attribute t <$> bitraverse f g e
+  bitraverse f g (FunctionExp fn) = FunctionExp <$> bitraverse f g fn
+  bitraverse f g (Array es) = Array <$> traverse (bitraverse f g) es
+  bitraverse f g (Call e as) = Call <$> bitraverse f g e
+    <*> traverse (bitraverse f g) as
+  bitraverse f g (Assign l e) = Assign <$> bitraverse f g l
+    <*> bitraverse f g e
+  bitraverse _ g (Other o) = Other <$> g o
+
 data Statement v o
   = Return (Expression v o)
   | Expression (Expression v o)
@@ -50,6 +74,16 @@ data Statement v o
   , Foldable
   , Traversable
   )
+
+instance Bifunctor Statement where
+  bimap = bimapDefault
+
+instance Bifoldable Statement where
+  bifoldMap = bifoldMapDefault
+
+instance Bitraversable Statement where
+  bitraverse f g (Return e) = Return <$> bitraverse f g e
+  bitraverse f g (Expression e) = Return <$> bitraverse f g e
 
 data LHS v o
   = LVariable v
@@ -63,6 +97,16 @@ data LHS v o
   , Traversable
   )
 
+instance Bifunctor LHS where
+  bimap = bimapDefault
+
+instance Bifoldable LHS where
+  bifoldMap = bifoldMapDefault
+
+instance Bitraversable LHS where
+  bitraverse f _ (LVariable v) = LVariable <$> f v
+  bitraverse f g (LAttribute t e) = LAttribute t <$> bitraverse f g e
+
 data Block v o = Block
   { provides   :: [v]
   , statements :: [Statement v o] }
@@ -74,6 +118,16 @@ data Block v o = Block
   , Foldable
   , Traversable
   )
+
+instance Bifunctor Block where
+  bimap = bimapDefault
+
+instance Bifoldable Block where
+  bifoldMap = bifoldMapDefault
+
+instance Bitraversable Block where
+  bitraverse f g (Block as ss) = Block <$> traverse f as
+    <*> traverse (bitraverse f g) ss
 
 data Function v o = Function
   { parameters :: [v]
@@ -87,6 +141,16 @@ data Function v o = Function
   , Foldable
   , Traversable
   )
+
+instance Bifunctor Function where
+  bimap = bimapDefault
+
+instance Bifoldable Function where
+  bifoldMap = bifoldMapDefault
+
+instance Bitraversable Function where
+  bitraverse f g (Function ps b) = Function <$> traverse f ps
+    <*> bitraverse f g b
 
 type RenderT m = ReaderT Int (WriterT Builder m)
 type Render = RenderT Identity
